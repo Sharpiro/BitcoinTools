743bf98bdc224cfafe703e16e722b2e3995803f3
sharpiro <dsharpbb09@gmail.com>
2017-08-30 11:28:32 -0400

    simplifying

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 var bitcore = require("bitcore-lib");
-// var randBuffer = bitcore.crypto.Random.getRandomBuffer(32);
-// var randNumber = bitcore.crypto.BN.fromBuffer(randBuffer);
-// var randNumber = "BF86625F2FA2F88E6C4ADAB0DD3AB49799B69BAEE843FDD115E27BAD320464B2";
-// var privateKey = new bitcore.PrivateKey(randNumber).toAddress();
-// var fakePrivateKey = new bitcore.PrivateKey(randNumber).toAddress("testnet");
-// var wifKey = bitcore.PrivateKey('testnet').toWIF();
 
-var wifKey2 = bitcore.PrivateKey.fromWIF("cTQfxsFs7NUtg42ahfg9UL1QAGG7adoGacFhLpXWBehVc68dRQg8");
-var address = wifKey2.toAddress();
+var privateKey = new bitcore.PrivateKey("testnet")
+// var privateKey = bitcore.PrivateKey.fromWIF(wifKey)
+var address = privateKey.toAddress()
+var wifKey = privateKey.toWIF();
 
-// console.log(randNumber);
-// console.log(privateKey);
-// console.log(fakePrivateKey);
-// console.log(wifKey);
-console.log(wifKey2);
+console.log(privateKey);
 console.log(address);
+console.log(wifKey);

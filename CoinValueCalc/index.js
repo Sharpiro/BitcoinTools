3b39ab814b0c6c54886635d56493e85584477634
sharpiro <dsharpbb09@gmail.com>
2018-01-03 13:57:54 -0500

    adding coin value calculator mini-app that calculates value based upon coincap

---
+++ CoinValueCalc/index.js
@@ -0,0 +1,79 @@
+var allCoinData
+
+refreshCoinData().then(data => {
+    console.log("coin data retrieved")
+    allCoinData = data
+})
+
+refresh.onclick = (event) => {
+    refreshCoinData().then(data => {
+        console.log("coin data retrieved")
+        allCoinData = data
+    })
+}
+
+submit.onclick = (event) => {
+    if (!allCoinData) {
+        console.log("no coin data")
+        return
+    }
+    if (!inputTextArea.value) {
+        console.log("no input data")
+        return
+    }
+    var data = inputTextArea.value
+    var split = data.split("\n").filter(d => d).map(d => d.split("\t"))
+    split = split.splice(1, split.length)
+    var consolidatedList = toConsolidatedList(split, allCoinData)
+    var viewList = consolidatedList.map(c => {
+        return {
+            short: c.short,
+            price: c.price,
+            myAmount: c.myAmount,
+            usdValue: c.usdValue,
+            dailyChange: c.cap24hrChange,
+            shapeshift: c.shapeshift
+        }
+    })
+    var totalValue = viewList.map(l => l.usdValue).reduce((prev, next) => prev + next)
+    var excelList = viewList.map((coin, index) => {
+        return (index + 1) + "\t" + coin.short + "\t" + coin.price + "\t" + coin.myAmount + "\t" + coin.usdValue + "\t" + coin.dailyChange + "\t" + coin.shapeshift
+    })
+    excelList.unshift("rank\tname\tprice\tmyAmount\tusdValue\tdailyChange\tshapeshift")
+    outputTextArea.innerText = excelList.join("\n")
+    totalValueElement.innerText = totalValue
+    console.log(viewList)
+    console.log(totalValue)
+}
+
+function refreshCoinData() {
+    return new Promise((res, rej) => {
+        var response = fetch("http://coincap.io/front")
+        var dataPromise
+        response.then(data => {
+            dataPromise = data.json()
+            dataPromise.then(json => {
+                res(json)
+            })
+        })
+    })
+}
+
+function toConsolidatedList(split, allCoinData) {
+    var dictionary = {}
+    var list = []
+    for (var item of split) {
+        if (dictionary[item[0]] == undefined) dictionary[item[0]] = 0
+        dictionary[item[0]] += parseFloat(item[1].replace(",", ""))
+    }
+
+    for (var item of allCoinData) {
+        if (dictionary[item.short] != undefined) {
+            item.myAmount = dictionary[item.short]
+            item.usdValue = item.myAmount * item.price
+            list.push(item)
+        }
+    }
+
+    return list
+}

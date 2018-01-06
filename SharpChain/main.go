92a91df83e29c374d8081d45aafbdf78978a3c38
sharpiro <dsharpbb09@gmail.com>
2018-01-05 20:26:13 -0500

    manual merge of mine function

--- SharpChain/main.go
+++ SharpChain/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"crypto/sha256"
+	"encoding/binary"
 	"encoding/hex"
 	"fmt"
+	"math"
 	"time"
 )
 
@@ -18,11 +21,22 @@ type block struct {
 var blockMap = map[string]*block{}
 
 func main() {
-	genesisBlock := createGenesisBlock("genesis")
-	blockMap[genesisBlock.hash] = genesisBlock
-	firstBlock := createBlock("first block", genesisBlock.hash)
-	blockMap[firstBlock.hash] = firstBlock
-	fmt.Println(blockMap)
+	// genesisBlock := createGenesisBlock("genesis")
+	// blockMap[genesisBlock.hash] = genesisBlock
+	// firstBlock := createBlock("first block", genesisBlock.hash)
+	// blockMap[firstBlock.hash] = firstBlock
+	// fmt.Println(blockMap)
+
+	data := "data"
+	difficulty := 3
+	now := time.Now()
+	nonce, hash := mine(data, difficulty)
+	elapsed := time.Since(now)
+	fmt.Println(elapsed)
+	hashesPerSecond := int(float64(nonce) / float64(elapsed.Seconds()))
+	fmt.Printf("%v hashes / second\n", hashesPerSecond)
+	fmt.Println(nonce)
+	fmt.Println(hash)
 }
 
 func createGenesisBlock(data string) *block {
@@ -52,4 +66,26 @@ func getHash(data string) string {
 	hasher.Write([]byte(data))
 	var hash = hex.EncodeToString(hasher.Sum(nil))
 	return hash
+}
+
+func mine(data string, difficulty int) (int, string) {
+	for nonce := 0; nonce < math.MaxInt32; nonce++ {
+		hasher := sha256.New()
+		buf := new(bytes.Buffer)
+		num := uint32(nonce)
+		_ = binary.Write(buf, binary.LittleEndian, num)
+		fakeData := []byte(data)
+		fakeData = append(fakeData, buf.Bytes()...)
+		hasher.Write(fakeData)
+		result := hasher.Sum(nil)
+		for j := 0; j < difficulty; j++ {
+			if result[j] != 0 {
+				break
+			}
+			if j == difficulty-1 {
+				return nonce, hex.EncodeToString(result)
+			}
+		}
+	}
+	return -1, ""
 }

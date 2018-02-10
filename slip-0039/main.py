5dea5ae06cc781aa3de04f85f51176c8639b1c22
sharpiro <dsharpbb09@gmail.com>
2018-02-10 13:42:35 -0500

    testingf

--- slip-0039/main.py
+++ slip-0039/main.py
@@ -20,9 +20,9 @@ import math
 # possible to our security level; e.g.  desired security level of 128
 # bits -- too large and all the ciphertext is large; too small and
 # security is compromised)
-# PRIME = 2**127 - 1
+PRIME = 2**127 - 1
 # PRIME = 0x11b
-PRIME = 0x11b
+# PRIME = 1613
 # 13th Mersenne Prime is 2**521 - 1
 
 randomInt = functools.partial(random.SystemRandom().randint, 0)
@@ -47,7 +47,8 @@ def make_random_shares(minimum, shares, prime=PRIME):
 
     if minimum > shares:
         raise ValueError("pool secret would be irrecoverable")
-    poly = [randomInt(prime) for i in range(minimum)]
+    # poly = [randomInt(prime) for i in range(minimum)]
+    poly = [randomInt(2**16) for i in range(minimum)]
     # poly = [1234, 166, 94]
     points = [(i, _eval_at(poly, i, prime)) for i in range(1, shares + 1)]
     return poly[0], points
@@ -62,14 +63,16 @@ def _extended_gcd(a, b):
         (old_r, r) = (r, old_r - quotient * r)
         (old_s, s) = (s, old_s - quotient * s)
         (old_t, t) = (t, old_t - quotient * t)
-    return old_s, r, old_t
+    results = old_s, r, old_t
+    return results
 
 
 def _egcd(a, b):
     if a == 0:
         return (b, 0, 1)
     g, y, x = _egcd(b % a, a)
-    return (g, x - (b // a) * y, y)
+    results = g, x - (b // a) * y, y
+    return results
 
 
 def _mod_inverse(k, prime):
@@ -80,44 +83,45 @@ def _mod_inverse(k, prime):
     else:
         _, _, r = _egcd(prime, k)
         # _, _, r = _extended_gcd(prime, k)
-    return (prime + r) % prime
-
-
-# def _lagrange_interpolate(x, x_s, y_s, prime):
-#     # break the points up into lists of x and y values
-#     # x_values, y_values = zip(*points)
-#     # initialize f(x) and begin the calculation: f(x) = SUM( y_i * l_i(x) )
-#     f_x = 0
-#     for i in range(len(x_s)):
-#         # evaluate the lagrange basis polynomial l_i(x)
-#         numerator, denominator = 1, 1
-#         # for j in x_s:
-#         for j in range(len(x_s)):
-#             # don't compute a polynomial fraction if i equals j
-#             if i == j:
-#                 continue
-#             # compute a fraction & update the existing numerator + denominator
-#             numerator = (numerator * (x - x_s[j])) % prime
-#             denominator = (denominator * (x_s[i] - x_s[j])) % prime
-#         # get the polynomial from the numerator + denominator mod inverse
-#         lagrange_polynomial = numerator * _mod_inverse(denominator, prime)
-#         # multiply the current y & the evaluated polynomial & add it to f(x)
-#         f_x = (prime + f_x + (y_s[i] * lagrange_polynomial)) % prime
-#     return f_x
+    modInverse = (prime + r) % prime
+    return modInverse
 
 
 def _lagrange_interpolate(x, x_s, y_s, prime):
+    # break the points up into lists of x and y values
+    # x_values, y_values = zip(*points)
+    # initialize f(x) and begin the calculation: f(x) = SUM( y_i * l_i(x) )
     f_x = 0
     for i in range(len(x_s)):
+        # evaluate the lagrange basis polynomial l_i(x)
         numerator, denominator = 1, 1
+        # for j in x_s:
         for j in range(len(x_s)):
+            # don't compute a polynomial fraction if i equals j
             if i == j:
                 continue
-            numerator = (numerator * (x - x_s[j]))
-            denominator = (denominator * (x_s[i] - x_s[j]))
-        lagrange_polynomial = numerator / denominator
-        f_x = (f_x + (y_s[i] * lagrange_polynomial))
-    return f_x % prime
+            # compute a fraction & update the existing numerator + denominator
+            numerator = (numerator * (x - x_s[j])) % prime
+            denominator = (denominator * (x_s[i] - x_s[j])) % prime
+        # get the polynomial from the numerator + denominator mod inverse
+        lagrange_polynomial = numerator * _mod_inverse(denominator, prime)
+        # multiply the current y & the evaluated polynomial & add it to f(x)
+        f_x = (prime + f_x + (y_s[i] * lagrange_polynomial)) % prime
+    return f_x
+
+
+# def _lagrange_interpolate(x, x_s, y_s, prime):
+#     f_x = 0
+#     for i in range(len(x_s)):
+#         numerator, denominator = 1, 1
+#         for j in range(len(x_s)):
+#             if i == j:
+#                 continue
+#             numerator = (numerator * (x - x_s[j]))
+#             denominator = (denominator * (x_s[i] - x_s[j]))
+#         lagrange_polynomial = numerator / denominator
+#         f_x = (f_x + (y_s[i] * lagrange_polynomial))
+#     return f_x % prime
 
 
 def _recover_secret(shares, prime=PRIME):
@@ -128,19 +132,14 @@ def _recover_secret(shares, prime=PRIME):
     return result
 
 
-# SECRET, SHARES = make_random_shares(minimum=3, shares=6)
-# print('secret and shares:', SECRET, SHARES)
-# should be 240, actually 51.3337
+SECRET, SHARES = make_random_shares(minimum=3, shares=6)
+print('secret and shares:', SECRET, SHARES)
 print('secret recovered from minimum subset of shares',
-      _recover_secret([(2, 37), (4, 50), (5, 279)]))
+      _recover_secret([SHARES[1], SHARES[3], SHARES[4]]))
 print('secret recovered from minimum subset of shares',
-      _recover_secret([(1, 253), (2, 37), (3, 158)]))
-# print('secret recovered from minimum subset of shares',
-#       _recover_secret([SHARES[1], SHARES[3], SHARES[4]]))
-# print('secret recovered from minimum subset of shares',
-#       _recover_secret(SHARES[:3]))
-# print('secret recovered from a different minimum subset of shares',
-#       _recover_secret(SHARES[-3:]))
+      _recover_secret(SHARES[:3]))
+print('secret recovered from minimum subset of shares',
+      _recover_secret(SHARES[-3:]))
 
 # secret = bytearray.fromhex(
 #     "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08")
@@ -164,3 +163,6 @@ print('secret recovered from minimum subset of shares',
 # p = 0b101100011  # 355   2^8 + 2^6 + 2^5 + 2^1 + 1
 # a = 0b1010100  # 84      2^6 + 2^4 + 2^2
 # b = 0b1101  # 13             2^3+2^2+2^0
+
+
+# print(_egcd(12, 8))
